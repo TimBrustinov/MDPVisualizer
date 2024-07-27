5fc5744540024e749d200aef5125b6b6d99e87e8
Tim Brustinov <timbrustinov@gmail.com>
2024-07-27 16:52:46 -0700

    Add project files.

---
+++ MDPVisualizer/Game1.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MDPVisualizer
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        private GridSquare[,] grid = new GridSquare[4, 3];
+        private Texture2D pixel;
+        private Point squareSize = new Point(100, 100);
+        private int spacing = 1;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new Color[] { Color.White });
+
+            for (int i = 0; i < grid.GetLength(0); i++)
+            {
+                for (int j = 0; j < grid.GetLength(1); j++)
+                {
+                    grid[i, j] = new GridSquare(pixel, new Vector2(i, j), new Rectangle(i * (squareSize.X + spacing), j * (squareSize.Y + spacing), squareSize.X, squareSize.Y), Color.White);
+                }
+            }
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (var item in grid)
+            {
+                item.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ MDPVisualizer/GridSquare.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MDPVisualizer
+{
+    public class GridSquare
+    {
+        public Vector2 Coordinates;
+        public Color Color;
+        private Texture2D texture;
+        public Rectangle boundingBox { get; private set; }
+
+        public GridSquare(Texture2D texture, Vector2 coordinates, Rectangle boundingBox, Color color)
+        {
+            this.texture = texture;
+            this.Coordinates = coordinates;
+            this.boundingBox = boundingBox;
+            this.Color = color;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, boundingBox, Color);
+        }
+    }
+}
